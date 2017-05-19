c6046d51c1a4877823261449008985a2a71c96ed
Roman Serikov <roman.serikov@yandex.ru>
2017-05-19 22:51:13 +0300

    Fix Lesson 2

--- lesson-02/shopping_sum.rb
+++ lesson-02/shopping_sum.rb
@@ -1,19 +1,29 @@
-cart       = {}
-cart_total = 0
-
-puts "Введите <название товара>, <цену за 1 шт> и <кол-во товара> через пробел."
-puts "Введите <стоп>, чтобы закончить."
+cart          = {}
+cart_total    = 0
+input_message = "Введите <название товара>, <цену за 1 шт> и <кол-во товара> через пробел.\nВведите <стоп>, чтобы закончить."
 
 loop do
+  puts input_message
   input = gets.chomp.split
 
   break if input == ["стоп"]
 
-  product  = input[0]
+  product  = input[0].downcase
   price    = input[1].to_f
   quantity = input[2].to_f
 
-  cart[product] = { price => quantity }
+  if cart.key?(product)
+    puts "Такой товар уже есть. Принять новые значения цены и кол-ва? <да> <нет>"
+    choice = gets.chomp
+    if choice == "да"
+      cart[product] = { price => quantity }
+      puts "Товар #{product} успешно изменен."
+    else
+      puts "Товар #{product} остался без изменений."
+    end
+  else
+    cart[product] = { price => quantity }
+  end
 end
 
 if cart.empty? 
--- lesson-02/vowels_hash.rb
+++ lesson-02/vowels_hash.rb
@@ -1,5 +1,5 @@
 vowels = {}
 
 ("a".."z").each.with_index(1) do |char, index|
-  vowels[char] = index if char =~ /[aeiou]/
+  vowels[char] = index if char =~ /[aeiouy]/
 end
